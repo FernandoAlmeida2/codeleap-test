724fcff863825fa0f52c4f30d96c8e980ea633ae
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2023-05-18 14:36:53 -0300

    chore: add function to get the appropriate time for posts

--- src/components/posts/Post/Post.js
+++ src/components/posts/Post/Post.js
@@ -4,18 +4,38 @@ import { PostBox, PostContent, PostInfo, TitleDiv } from "./Styles";
 export default function Post({ post }) {
   const { username, created_datetime, title, content } = post;
 
-  const time_post = dayjs().diff(created_datetime, 'minute');
+  const time_post = getTimePost(created_datetime);
 
-  return(
+  return (
     <PostBox>
-        <TitleDiv>
-            <h1>{title}</h1>
-        </TitleDiv>
-        <PostInfo>
-            <h2>@{username}</h2>
-            <h3>{time_post} minutes ago</h3>
-        </PostInfo>
-        <PostContent>{content}</PostContent>
+      <TitleDiv>
+        <h1>{title}</h1>
+      </TitleDiv>
+      <PostInfo>
+        <h2>@{username}</h2>
+        <h3>{time_post}</h3>
+      </PostInfo>
+      <PostContent>{content}</PostContent>
     </PostBox>
-  )
+  );
+}
+
+function getTimePost(datetime) {
+  const timeDiff = dayjs().diff(datetime, "second");
+  if (timeDiff < 60) {
+    return `${timeDiff} seconds ago`;
+  }
+  if (timeDiff < 120) {
+    return `${Math.round(timeDiff / 60)} minute ago`;
+  }
+  if (timeDiff < 3600) {
+    return `${Math.round(timeDiff / 60)} minutes ago`;
+  }
+  if (timeDiff < 7200) {
+    return `${Math.round(timeDiff / 60)} hour ago`;
+  }
+  if (timeDiff < 86400) {
+    return `${Math.round(timeDiff / 60)} hours ago`;
+  }
+  return dayjs(datetime).format("MM/DD/YYYY");
 }
